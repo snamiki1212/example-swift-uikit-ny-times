36122bf008bb2eedef901b9d7a0e0fe2fb53af4e
snamiki1212 <snamiki1212@gmail.com>
2021-06-10 15:17:36 -0700

    Add skelton

---
+++ nytimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nytimes
+//
+//  Created by shunnamiki on 2021/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
