87ab386951c096efa7e466dc37c7a982e80820ff
snamiki1212 <snamiki1212@gmail.com>
2021-06-14 18:40:32 -0700

    Update ui-image-view

---
+++ nytimes/UIImageView.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView.swift
+//  nytimes
+//
+//  Created by shunnamiki on 2021/06/14.
+//
+
+import Foundation
+import UIKit
+
+// REF: https://www.hackingwithswift.com/example-code/uikit/how-to-load-a-remote-image-url-into-uiimageview
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            guard let data = try? Data(contentsOf: url),
+                  let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+    }
+}
