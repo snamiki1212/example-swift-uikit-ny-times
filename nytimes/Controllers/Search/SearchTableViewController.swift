a9438f65c14a6380ec6cd6d4e06f3a5321a2ed02
snamiki1212 <snamiki1212@gmail.com>
2021-06-17 12:20:22 -0700

    Refactor logics

--- nytimes/Controllers/Search/SearchTableViewController.swift
+++ nytimes/Controllers/Search/SearchTableViewController.swift
@@ -41,42 +41,26 @@ class SearchTableViewController: UITableViewController{
         tableView.refreshControl = UIRefreshControl()
     }
     
-    var response: Response? // TODO: removeResponse
-    
-    func updateUIView (){
-        print("TODO: UPDATE UI VIEW", response)
-    }
-    
-//    func diffWithSearchedResponses(_ list: [Response]) -> [Response] {
-//        if fetchedList.count == 0 {
-//            return list
-//        }
-//        return fetchedList.difference(from: list)
-////        return fetchedList.count == 0 ? list : fetchedList.difference(from: list)
-//    }
-    
     func search (){
-        print("START search")
         guard let searchText = self.searchText else {
             refreshControl?.endRefreshing()
             return
         }
         
         SearchRequest().search(searchText: searchText) { result in
-            switch result {
-            case .success(let res):
-                self.response = res
-            case .failure:
-                self.response = nil
-            }
+            self.fetchedList = {
+                switch result {
+                case .success(let res):
+                    return res.response.docs
+                case .failure:
+                    return []
+                }
+            }()
             DispatchQueue.main.async {
-                self.updateUIView()
-                self.fetchedList = self.response?.response.docs ?? []
                 self.tableView.reloadData()
                 self.refreshControl?.endRefreshing()
             }
         }
-        
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -91,7 +75,6 @@ class SearchTableViewController: UITableViewController{
         let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.reuseIdentifier, for: indexPath) as! SearchTableViewCell
         let item = fetchedList[indexPath.row]
         cell.item = item
-        
         return cell
     }
     
